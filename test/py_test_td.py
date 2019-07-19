29fb7faa56aa57de6125a8dc9e1eb67faee51551
shifulin <s.flin@foxmail.com>
2019-07-19 21:44:14 +0800

    修改细节

--- test/py_test_td.py
+++ test/py_test_td.py
@@ -1,4 +1,5 @@
 import time
+from ctypes import c_char_p
 
 from api.ctp.td.ctp_td import CTPTd
 from api.api_define import CTP_D_BUY, CTP_OF_OPEN, CTP_OPT_LIMIT_PRICE
@@ -8,9 +9,10 @@ ctp_td = CTPTd()
 
 if __name__ == '__main__':
     print(ctp_td.so.is_api_ok())
-    order_ref = ctp_td.insert_order('al1909', 'SHFE', CTP_D_BUY, CTP_OF_OPEN, CTP_OPT_LIMIT_PRICE, 13165.0, 1)
+    exchange = c_char_p(b'SHFE')
+    order_ref = ctp_td.insert_order('al1909', exchange, CTP_D_BUY, CTP_OF_OPEN, CTP_OPT_LIMIT_PRICE, 13500.0, 1)
     time.sleep(10)
-    ctp_td.delete_order('al1909', 'SHFE', order_ref)
+    ctp_td.delete_order('al1909', exchange, order_ref)
     while input() != 'q':
         pass
 
